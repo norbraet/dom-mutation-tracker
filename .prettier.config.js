3a8a9987204501667ab46f014ee8b5324d2ba55b
Norbert Balaz <norbert.balaz@odacer.com>
2026-01-16 22:47:54 +0100

    chore(Setup): finished project setuo

---
+++ .prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+export default {
+  semi: false,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  trailingComma: "es5",
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 100,
+  endOfLine: "lf",
+};
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dom-mutation-tracker
+
 A Chrome DevTools snippet that tracks DOM mutations, logs them cleanly in the console, and visually highlights changed elements in real time.
